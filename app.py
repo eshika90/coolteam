d60995b786a8e83ed96a8d17b1580e8406626fbc
Kim Seryung <129057141+eshika90@users.noreply.github.com>
2023-05-17 20:43:31 +0900

    bucket-list 삭제구현
    bucket-list 삭제구현

--- app.py
+++ app.py
@@ -9,6 +9,33 @@ db = client.dbsparta
 def home():
    return render_template('index2.html')
 
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+
+    bucket_list = list(db.bucket.find({},{'_id':False}))
+    count = len(bucket_list) + 1
+    doc = {
+        'num': count,
+        'bucket': bucket_receive
+    }
+    db.bucket.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    all_bucket = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'result':all_bucket})
+
+@app.route('/bucket/delete', methods=['POST'])
+def complete_bucket():
+    delete_receive = request.form['delete_give']
+
+    db.bucket.delete_one({'num': int(delete_receive)})
+
+    return jsonify({'msg': '수고하셨습니다!'})
+
 @app.route("/guestbook", methods=["POST"])
 def guestbook_post():
     name_receive = request.form['name_give']
@@ -19,8 +46,7 @@ def guestbook_post():
     doc = {
         'num': count,
         'name': name_receive,
-        'comment': comment_receive,
-        'done':0
+        'comment': comment_receive
     }
     db.fan.insert_one(doc)
 
@@ -37,7 +63,7 @@ def delete_star():
 
     db.fan.delete_one({'num': int(delete_receive)})
 
-    return jsonify({'msg': 'delete 연결되었습니다!'})
+    return jsonify({'msg': '삭제 완료!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
